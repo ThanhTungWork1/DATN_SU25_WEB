640abf8ffda5d5ded7b381b0c4c9513fac4279f5
hungtrinh245 <trinhquochung222005@gmail.com>
2025-06-18 00:37:04 +0700

    assign icons to sidebar categories

--- Frontend/my-app/src/App.tsx
+++ Frontend/my-app/src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import logo from "./assets/logo.png";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { adminRoutes } from "./routes/adminRoutes";
 import "antd/dist/reset.css";
 import { Layout, Menu } from "antd";
+import {
+  ShoppingOutlined,
+  ShoppingCartOutlined,
+  LoginOutlined,
+} from "@ant-design/icons";
 
 const { Header, Content, Sider } = Layout;
 
@@ -11,18 +18,23 @@ function App() {
     <BrowserRouter>
       <Layout style={{ minHeight: "100vh" }}>
         <Sider breakpoint="lg" collapsedWidth="0">
-          <div style={{ color: "white", padding: 20, fontSize: 18 }}>Admin Panel</div>
+        <div style={{ textAlign: "center", padding: 20 }}>
+  <img src={logo} alt="Admin Panel" style={{ width: 45, borderRadius: 8 }} />
+  <div style={{ color: "white", marginTop: 8, fontWeight: "bold" }}>Admin Panel</div>
+</div>
+
           <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-            <Menu.Item key="1">
-              <a href="/admin/products">Sản phẩm</a>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <a href="/admin/orders">Đơn hàng</a>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <a href="/login">Đăng nhập</a>
-            </Menu.Item>
-          </Menu>
+  <Menu.Item key="1" icon={<ShoppingOutlined />}>
+    <a href="/admin/products">Sản phẩm</a>
+  </Menu.Item>
+  <Menu.Item key="2" icon={<ShoppingCartOutlined />}>
+    <a href="/admin/orders">Đơn hàng</a>
+  </Menu.Item>
+  <Menu.Item key="3" icon={<LoginOutlined />}>
+    <a href="/login">Đăng nhập</a>
+  </Menu.Item>
+</Menu>
+
         </Sider>
         <Layout>
           <Header style={{ background: "#fff", padding: 0 }} />
