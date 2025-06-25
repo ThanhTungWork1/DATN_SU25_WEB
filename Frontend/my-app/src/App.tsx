e2f2332008703b648653ef936cea795a104b9abb
ThanhTungWork1 <thanhtungworkup@gmail.com>
2025-06-25 16:31:15 +0700

    userProfile

--- Frontend/my-app/src/App.tsx
+++ Frontend/my-app/src/App.tsx
@@ -2,15 +2,16 @@ import ClientRoute from "./routes/ClientRoute";
 import { Toaster } from "sonner";
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import AdminRoute from './routes/AdminRoute'
+import UserProfile from './pages/client/userProfile'
 
 function App() {
 
   return (
     <>
-      <ClientRoute />
-      <Toaster position="top-right" richColors />
-    <AdminRoute/>
+    <Routes>
+        {/* Các route khác ở đây */}
+        <Route path="/users/:id" element={<UserProfile />} />
+    </Routes>
     </>
   );
 }
--- Frontend/my-app/src/main.tsx
+++ Frontend/my-app/src/main.tsx
@@ -1,31 +1,13 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-// import './index.css'
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
-import ReactQueryProvider from "./provider/ClientProvideDetail.tsx";
-import { CartProvider } from "./provider/CartProvider";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ReactQueryProvider>
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./index.css";
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <QueryClientProvider client={queryClient}>
         <BrowserRouter>
-          <CartProvider>
             <App />
-          </CartProvider>
         </BrowserRouter>
-      </ReactQueryProvider>
-    </Provider>
-  </React.StrictMode>
+    </QueryClientProvider>
 );
-
