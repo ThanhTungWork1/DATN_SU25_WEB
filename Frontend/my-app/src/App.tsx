6b6b22e60162c48be28c0d189a3805a634ad5e0f
ThanhTungWork1 <thanhtungworkup@gmail.com>
2025-06-26 08:14:39 +0700

    Auth

--- Frontend/my-app/src/App.tsx
+++ Frontend/my-app/src/App.tsx
@@ -1,17 +1,24 @@
-import ClientRoute from "./routes/ClientRoute";
-import { Toaster } from "sonner";
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import UserProfile from './pages/client/userProfile'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import HomePage from './pages/client/Home'
+import Header from './components/Home/Header'
+import Footer from './components/Home/Footer'
 
 function App() {
 
   return (
     <>
+    <Header/>
     <Routes>
-        {/* Các route khác ở đây */}
+        <Route path='/' element={<HomePage/>}/>
         <Route path="/users/:id" element={<UserProfile />} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
     </Routes>
+    <Footer/>
     </>
   );
 }
